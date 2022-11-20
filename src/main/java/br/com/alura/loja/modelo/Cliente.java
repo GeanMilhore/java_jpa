4b5e6cff7e63eb2325f1897f4aa396928ea642a2
Gean <gean.milhore@gmail.com>
2022-11-20 19:57:39 -0300

    aula 5 - aprendendo conceitos de embeddable e embedded

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,14 +14,18 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private String cpf;
+	
+	@Embedded
+	private DadosPessoais dadosPessoais;
 
 	public Cliente() {}
 	
 	public Cliente(String nome, String cpf) {
-		this.nome = nome;
-		this.cpf = cpf;
+		this.dadosPessoais = new DadosPessoais(nome,cpf);
+	}
+	
+	public String getNome() {
+		return this.dadosPessoais.getNome();
 	}
 	
 	public Long getId() {
@@ -31,19 +36,7 @@ public class Cliente {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
 }
